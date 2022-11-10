a04fc4a06033b6436311e19a95206064c2ed3e58
Andrei Klimenok <jaguarislove@gmail.com>
2022-11-10 18:55:13 +0100

    Refactoring

--- homework02.py
+++ homework02.py
@@ -1,50 +1,64 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+# Create data set
 def create_dataset():
     x = np.random.rand(100)
-    x = x.reshape(x.shape[0], 1)
-    # t = x**3-x**2 + 1
     t = x**2
-    # plt.scatter(x, t, s=5)
-    # plt.show()
+    x = [x.reshape(1, 1) for x in x]
+    t = [t.reshape(1, 1) for t in t]
+
+    # Plot to show how the function is supposed to look like
+    plt.scatter(x, t, s=5)
+    plt.title("The function to be approximated")
+    plt.ylabel("target")
+    plt.show()
     return (x, t)
 
+# Shuffles dataset
+def shuffle(x,t):
+    indices = np.arange(x.shape[0])
+    np.random.shuffle(indices)
+
+    x = x[indices]
+    t = t[indices]
+
+    return x,t
+
+# Relu activation function
 def relu(x):
     return np.maximum(0, x)
 
+# Derivative of relu
 def d_relu(x):
     return (x > 0) * 1
 
+# Mean squared error
 def mse(y, t):
     return (y - t)**2 / 2
 
+# Derivative of the mean squared error
 def d_mse(y, t):
     return y - t
 
 class Layer:
     def __init__(self, n_inputs, n_units):
-        self.biases = np.ones((n_units, 1))
+        self.biases = np.zeros((n_units, 1))
         self.weights = np.random.rand(n_inputs, n_units)
         self.n_units = n_units
         self.input, self.preactivation, self.activation = None, None, None
 
     def forward_step(self, input):
-        self.input = input.reshape(input.shape[0], 1)
+        self.input = input
         self.preactivation = self.weights.T @ self.input + self.biases
         self.activation = relu(self.preactivation)
-        # print('FORWARD STEP:')
-        # print('Input: ', self.input)
-        # print('Weights: ', self.weights)
-        # print('Biases: ', self.biases)
-        # print('Preactivation: ', self.preactivation)
         return self.activation
     
     def backward_step(self, output, learning_rate):
         gradient_loss_over_activation = output
         gradient_activation_over_preactivation = d_relu(self.preactivation)
         gradient_preactivation_over_weights = self.input
-        gradient_preactivation_over_bias = np.zeros((self.n_units, 1))
+        gradient_preactivation_over_bias = np.ones((self.n_units, 1))
         gradient_preactivation_over_input = self.weights
 
         gradient_loss_over_preactivation = gradient_activation_over_preactivation * gradient_loss_over_activation
@@ -70,27 +84,35 @@ class MLP:
         for layer in reversed(self.layers):
             output = layer.backward_step(output, self.learning_rate)
 
+    def train(self, x, t):
+        y = self.forward_step(x)
+        loss = mse(y, t)
+        self.backpropagation(d_mse(y, t))
+        return loss
+
 
 def main():
+    epoch_size = 100
     x, t = create_dataset()
     mlp = MLP([Layer(1, 10), Layer(10, 10), Layer(10, 1)], learning_rate=0.001)
-    epochs = 100
+    mean_loss_per_epoch = []
     losses = []
-    for e in range(epochs):
+    for e in range(epoch_size):
+        # x,t = shuffle(x,t)
+        # mean_loss = mlp.train(x, t)
+        # mean_loss_per_epoch.append(mean_loss)
         for (i, j) in zip(x, t):
-            y = mlp.forward_step(i)
-            loss = mse(y, j)
+            loss = mlp.train(i, j)
             losses.append(loss)
-            mlp.backpropagation(d_mse(j, y))
-        print(f"Epoch: {e + 1}, Loss: {losses[-1]}")
 
-    predictions = []
-    for a in x:
-        y = mlp.forward_step(a)
-        predictions.append(y)
-    plt.scatter(x, predictions)
+        mean_loss_per_epoch.append(np.mean(losses))
+
+    # Plot the mean loss per epoch
+    plt.plot(range(epoch_size), mean_loss_per_epoch)
+    plt.title("Mean loss per Epoch")
+    plt.xlabel("epoch")
+    plt.ylabel("mean loss")
     plt.show()
-
 
 if __name__ == "__main__":
     main()
